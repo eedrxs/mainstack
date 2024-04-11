814069a27b7fd28a07cf59be4eaa7748fdff1f7e
eedris <technocr.aterpdevelopers@gmail.com>
2024-04-11 18:53:51 +0100

    quick edit

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -1,13 +1,8 @@
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"
 
 const Apps = () => {
   redirect("/apps/link-in-bio")
-
-  return (
-    <div className="font-black text-2xl">
-      Apps
-    </div>
-  );
+  return <div className="font-black text-2xl">Apps</div>
 }
 
-export default Apps;
+export default Apps
